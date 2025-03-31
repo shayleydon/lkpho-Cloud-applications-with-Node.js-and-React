90d84cfff288474a70f0793376c1f276601ee9c2
shayleydon <shayleydon>
2025-03-31 10:33:17 +0100

    debug custom greeting

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -7,6 +7,23 @@ const today = require('./today');
 // Define the request listener function
 const requestListener = function (req, res) {
     res.writeHead(200); // Set the status code to 200 (OK)
+
+    let dateVal = today.getDate(); // Get the current date from the 'today' module
+    // console.log(today); // Log the date to the console
+     // Determine the appropriate greeting based on the current time
+    // let greeting = "It is still not morning";
+    // if (dateVal.getHours() > 6 && dateVal.getHours() < 12) {
+    //     greeting = "Good morning!";
+    // } else if (dateVal.getHours() >= 12 && dateVal.getHours() < 18) {
+    //     greeting = "Good afternoon!";
+    // } else if (dateVal.getHours() >= 18 && dateVal.getHours() < 21) {
+    //     greeting = "Good evening!";
+    // } else if (dateVal.getHours() >= 21 && dateVal.getHours() < 24) {
+    //     greeting = "Good night!";
+    // }
+        
+    // Send the response with the appropriate greeting
+    // res.end(`Hello, ${greeting}`);
     // Send the response with the current date from the 'today' module
     res.end(`Hello, World! The date today is ${today.getDate()}`);
 };
